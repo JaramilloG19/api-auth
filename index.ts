65b6bd44422e220ebc8f980e2bc8f79e8181be20
JaramilloG19 <andres.jaramillo@masu.mx>
2023-08-09 21:22:45 -0500

    feat: implement morgan

--- index.ts
+++ index.ts
@@ -1,13 +1,18 @@
-import 'dotenv/config';
-import cors from 'cors';
-import express from 'express';
-import { dbConnect } from './src/config/mongoose';
-const port = process.env.PORT || 3000;
-const app = express();
-app.use(cors());
-app.use(express.json());
-app.listen(port, () => {
-  console.log(`Server is  running on por ${port} 🚀`);
-});
+import 'dotenv/config'
+import cors from 'cors'
+import morgan from 'morgan'
+import express from 'express'
+import router from './src/routes/index.r'
+import { dbConnect } from './src/config/mongoose'
+const port = process.env.PORT ?? 3000
+const app = express()
+app.use(cors())
+app.use(morgan('dev'))
+app.use(express.json())
+app.use('/api', router)
 
-dbConnect();
+app.listen(port, () => {
+  console.log(`Server is  running on por ${port} 🚀`)
+})
+
+void dbConnect()
