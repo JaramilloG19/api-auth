adbb0d5f7aa79bf4c93a27efa09b069579e85e5a
JaramilloG19 <andres.jaramillo@masu.mx>
2023-08-09 21:22:01 -0500

    feat: implement connect mongoose

--- src/config/mongoose.ts
+++ src/config/mongoose.ts
@@ -1,11 +1,12 @@
-import mongoose from 'mongoose';
+import mongoose from 'mongoose'
 
 export const dbConnect = async (): Promise<void> => {
-  const DB_URI = process.env.DB_URI || '';
+  const DB_URI = process.env.DB_URI ?? ''
   try {
-    await mongoose.connect(DB_URI, {});
-    console.log('Connected to database');
+    await mongoose.connect(DB_URI, {})
+    console.log('Connected to database')
   } catch (error) {
-    throw new Error(`Error connecting to database: ${error}`);
+    console.log('🚀 ~ method dbConnected ~ error', error)
+    throw error
   }
-};
+}
