5e64ea7b02ab2adfc744f5b457d959d6858c4622
JaramilloG19 <JaramilloG1999@outlook.com>
2023-08-12 17:15:07 -0500

    feat: adding comments

--- src/utils/files.ts
+++ src/utils/files.ts
@@ -1,3 +1,3 @@
-export const removeExtension = (filename: string): string => {
-  return filename.split('.').shift() as string
+export const removeExtension = (filename: string): string => { //  filename = 'get.r.ts'
+  return filename.split('.').shift() as string // filename.split('.') = ['get', 'r', 'ts']
 }
