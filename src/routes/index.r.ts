674c4a2a5bfa5ef4058d290f558cd65d67640094
JaramilloG19 <JaramilloG1999@outlook.com>
2023-08-12 14:39:00 -0500

    feat: adding comments in index routes dynamic import

--- src/routes/index.r.ts
+++ src/routes/index.r.ts
@@ -5,8 +5,8 @@ const PATH_ROUTES = __dirname
 const router = express.Router()
 
 fs.readdirSync(PATH_ROUTES).filter(async (file): Promise<void> => {
-  const nameRouter = removeExtension(file)
-  nameRouter !== 'index' && router.use(`/${nameRouter}`, await import(`./${file}`).then(mod => mod.default))
+  const nameRouter = removeExtension(file) // nameRouter = ['get', 'user', 'post', 'path', 'index']
+  nameRouter !== 'index' && router.use(`/${nameRouter}`, await import(`./${file}`).then(mod => mod.default)) //
 })
 
 export default router
