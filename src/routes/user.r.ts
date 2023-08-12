3a92407226317ac2341989b5860927be5e8707d8
JaramilloG19 <JaramilloG1999@outlook.com>
2023-08-12 14:38:39 -0500

    feat: remove user controller, model and routes why not necessary

--- src/routes/user.r.ts
+++
@@ -1,8 +0,0 @@
-/* eslint-disable @typescript-eslint/no-misused-promises */
-import express from 'express'
-import { getUser } from '../controllers/user.c'
-const router = express.Router()
-
-router.get('/', getUser)
-
-module.exports = router
